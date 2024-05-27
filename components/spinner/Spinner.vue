f3e6217fab80ddd61351abc0ca33ed6d16acd64c
Felipe Pereira <luisfelipelfsp3@gmail.com>
2024-05-27 14:19:53 +0000

    :tada: Initial

---
+++ components/spinner/Spinner.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ChevronRight } from "lucide-vue-next";
+
+const props = defineProps<{
+	class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+	<ChevronRight :class="cn('size-4', props.class)" />
+</template>
---
+++ pages/shop/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>shop</h1>
+</template>
